be7838be8cf00c44a1d3a2fa57bada5725fa2aa7
payose <inipayose@gmail.com>
2021-01-07 22:16:21 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,52 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+   <header>
+     <router-link to="/">
+      <h1><span>Dao</span>Movies</h1>
+     </router-link>
+   </header>
+
+  <main>
+    <router-view/>
+  </main>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+*{
+  margin:0;
+  padding:0;
+  box-sizing: border-box;
+  font-family:'Fira Sans', sans-serif;
+
+  &::selection{
+    background: transparentize(#c6ea0c, 0.6);
+  }
 }
 
-#nav {
-  padding: 30px;
+body{
+  background-color: #339621;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+a{
+  text-decoration: none;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
+header{
+  display:flex;
+  align-items: center;
+  justify-content: center;
+  padding: 10px 16px;
+  background-color: rgba(17, 16, 15, 0.966);
+  box-shadow: 0px 0px 6px rgba(40, 85, 3, 0.938);
+
+  h1{
+    color: #fbfcfa;
+    font-size: 28px;
+
+    span{
+      color: #185808;
     }
   }
 }
+
+
 </style>
---
+++ src/env.js
@@ -0,0 +1,3 @@
+export default{
+    apikey:"ea56134e"
+}
